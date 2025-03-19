516ab94d19e4a6178f6511a976edab7979730a69
Irshad <alamirshad1366@gmail.com>
2025-03-19 18:21:17 +0530

    jenkinsfile added

--- src/main/java/com/irshadit/App.java
+++ src/main/java/com/irshadit/App.java
@@ -5,6 +5,15 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("This is my world");
 		System.out.println("xyzndjkd");
+
+		//task-1
+
+		int i;
+		int j;
+		System.out.println(i+j);
+
+
+
 	}
 
 }
